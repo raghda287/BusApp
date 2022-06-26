54b1e5352adde5a8abec18f94e958f1645dfa890
raghda287 <70110804+raghda287@users.noreply.github.com>
2022-06-26 17:04:23 +0200

    Update states.dart

---
+++ lib/modules/auth/widgets/login/states.dart
@@ -0,0 +1,19 @@
+import 'package:easy_bus_app/models/login/user_login.dart';
+
+abstract class UserLoginStates {}
+
+class UserLoginInitState extends UserLoginStates {}
+
+class UserLoginLoadingState extends UserLoginStates {}
+
+class UserLoginSuccessState extends UserLoginStates {
+  final UserLoginModel loginModel;
+
+  UserLoginSuccessState(this.loginModel);
+}
+
+class UserLoginErrorState extends UserLoginStates {
+  final String error;
+
+  UserLoginErrorState(this.error);
+}
